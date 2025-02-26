1ec0ef44feb80a3816078cf5fc56c68e4fc160ac
ppippi804 <83631862+ppippi804@users.noreply.github.com>
2025-02-26 22:27:23 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+// const text = "안녕하세요 한덕수 박정민 인사드립니다. 지금부터 모바일 청첩장 시작합니다."; // 타이핑할 문구
+// let index = 0;
+// let speed = 100; // 글자 타이핑 속도 (밀리초 단위)
+
+// function typeWriter() {
+//   if (index < text.length) {
+//     document.getElementById("type").textContent += text.charAt(index);
+//     index++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
+
+// typeWriter();
